c11d8660c0d2a5ecf1221e2488ac80eaf39a0d22
Saulius Milevicius <smilev2@uic.edu>
2013-04-28 20:44:08 -0500

    cpp comment

--- ROSIEProject/main.cpp
+++ ROSIEProject/main.cpp
@@ -7,11 +7,10 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    //Load the QML project
     QtQuick2ApplicationViewer viewer;
     viewer.setMainQmlFile(QStringLiteral("qml/ROSIEProject/ROSIE.qml"));
     viewer.showExpanded();
-
-
 
     return app.exec();
 }
